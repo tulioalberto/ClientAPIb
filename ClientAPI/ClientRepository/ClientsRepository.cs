4b733d7ae0d1e71dfe32f1beaf59768a5a51f6a4
Tulio <Tulio@WIN-2KB4VA1LDT0>
2022-10-14 12:32:45 -0300

    Adicionar arquivos de projeto.

---
+++ ClientAPI/ClientRepository/ClientsRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClientAPI.Models;
+using ClientAPI.ClientContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientAPI.ClientRepository
+{
+	public class ClientsRepository : IClientRepository
+	{
+		private readonly ClientsContext _context;
+
+		public ClientsRepository(ClientsContext context)
+		{
+			_context = context;
+		}
+		public async Task<Client> Create(Client client)
+		{
+			_context.clients.Add(client);
+			await _context.SaveChangesAsync();
+			return client;
+		}
+
+		public async Task<Client> Delete(int Id)
+		{
+			var clientToDelete = await _context.clients.FindAsync(Id);
+			_context.clients.Remove(clientToDelete);
+			await _context.SaveChangesAsync();
+			return null;
+		}
+
+		public async Task<List<Client>> Get()
+		{
+			return await _context.clients.ToListAsync();
+		}
+
+		public async Task<Client> Get(int Id)
+		{
+			return await _context.clients.FindAsync(Id);
+		}
+
+		public async Task<Client> Update(Client client)
+		{
+			_context.Entry(client).State = EntityState.Modified;
+			await _context.SaveChangesAsync();
+			return null;
+		}
+	}
+}
---
+++ ClientAPI/Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClientAPI.ClientRepository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+
+namespace ClientAPI
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddControllers();
+			services.AddTransient<MySqlConnection>(_ => new MySqlConnection(Configuration["ConnectionStrings:Default"]));
+			services.AddTransient<IClientRepository, ClientsRepository>();
+			services.AddSwaggerGen();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+
+			app.UseSwagger(c =>
+			{
+				c.SerializeAsV2 = true;
+			});
+			app.UseSwaggerUI(c => {
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+			});
+		}
+	}
+}
